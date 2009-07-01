a2e69d6129d4ae02f5e6fd9e4741a0dfa3d3fbd4
Bryant Luk <bluk@apache.org>
2009-07-01 00:57:23 +0000

    test framework with geronimo and jetty for WINK-16
    wink-integration-test module not currently
    included in main pom.xml but can be added later.
    
    By default, geronimo-jetty version is used
    which will install Geronimo (if available
    on your system), and then go through all
    submodules and deploy the applications.
    
    Providers test is just a dummy one for now
    
    mvn install should be called from main
    wink-integration-test/pom.xml to do everything.
    
    Based off Geronimo test suite setup as suggested
    by Jason.
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wink/trunk@790010 13f79535-47bb-0310-9956-ffa450edef68

---
+++ wink-integration-test/wink-server-integration-test-support/src/main/java/org/apache/wink/test/integration/ServerEnvironmentInfo.java
@@ -0,0 +1,46 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.wink.test.integration;
+
+/**
+ * This class provides some basic information for clients about the server.
+ */
+final public class ServerEnvironmentInfo {
+
+    public static String getHostname() {
+        return System.getProperty("wink-test-hostname");
+    }
+
+    public static String getPort() {
+        return System.getProperty("wink-test-port");
+    }
+
+    public static String getContextRoot() {
+        return System.getProperty("wink-test-context-root");
+    }
+
+    public static String getBaseURI() {
+        String contextRoot = getContextRoot();
+        if (contextRoot == null) {
+            return "http://" + getHostname() + ":" + getPort();
+        }
+        return "http://" + getHostname() + ":" + getPort() + "/" + contextRoot;
+    }
+}
