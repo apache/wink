58815b62add4e48fcd6cca9354c9cafb31a50463
Bryant Luk <bluk@apache.org>
2009-09-25 20:58:48 +0000

    Add example to itests to gather trace temporarily
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wink/trunk@819007 13f79535-47bb-0310-9956-ffa450edef68

---
+++ wink-itests/wink-itest/wink-example-helloworld/src/test/java/org/apache/wink/example/helloworld/HelloWorldTest.java
@@ -0,0 +1,62 @@
+/*******************************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *  
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ *  
+ *******************************************************************************/
+
+package org.apache.wink.example.helloworld;
+
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.methods.GetMethod;
+import org.apache.wink.test.integration.ServerEnvironmentInfo;
+
+import junit.framework.TestCase;
+
+/**
+ * Test of response from HelloWorld Resource.
+ */
+public class HelloWorldTest extends TestCase {
+
+    private static String getBaseURI() {
+        if (ServerEnvironmentInfo.isRestFilterUsed()) {
+            return ServerEnvironmentInfo.getBaseURI();
+        }
+        return ServerEnvironmentInfo.getBaseURI() + "/hello";
+    }
+
+    private HttpClient client;
+
+    public void setUp() {
+        client = new HttpClient();
+    }
+
+    /**
+     * Tests that a Hello World app will come up.
+     * 
+     * @throws Exception
+     */
+    public void testHelloWorld() throws Exception {
+        GetMethod getMethod = new GetMethod(getBaseURI() + "/world");
+        try {
+            client.executeMethod(getMethod);
+            assertEquals(200, getMethod.getStatusCode());
+            assertTrue(getMethod.getResponseBodyAsString(), getMethod.getResponseBodyAsString().contains("Hello World"));
+        } finally {
+            getMethod.releaseConnection();
+        }
+    }
+}
