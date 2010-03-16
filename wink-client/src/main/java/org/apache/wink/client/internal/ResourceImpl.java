0a3afb78b8eb913c888fbfa805b1f3be95b44f15
Bryant Luk <bluk@apache.org>
2010-03-16 18:29:49 +0000

    Update client version
    Doing it now so don't forget later.
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wink/trunk@923924 13f79535-47bb-0310-9956-ffa450edef68

--- wink-client/src/main/java/org/apache/wink/client/internal/ResourceImpl.java
+++ wink-client/src/main/java/org/apache/wink/client/internal/ResourceImpl.java
@@ -55,7 +55,7 @@ import org.slf4j.LoggerFactory;
 
 public class ResourceImpl implements Resource {
 
-    private static final String            USER_AGENT = "Wink Client v1.0";                         //$NON-NLS-1$
+    private static final String            USER_AGENT = "Wink Client v1.1";                         //$NON-NLS-1$
 
     private ProvidersRegistry              providersRegistry;
     private ClientConfig                   config;
