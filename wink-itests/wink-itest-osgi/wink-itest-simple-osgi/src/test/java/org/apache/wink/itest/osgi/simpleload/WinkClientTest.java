f0cd7a8194d9215fafb26466f828946ecd773496
Bryant Luk <bluk@apache.org>
2010-03-29 22:37:46 +0000

    Add OSGi metadata to JARs
    See [WINK-263]
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wink/trunk@928925 13f79535-47bb-0310-9956-ffa450edef68

---
+++ wink-itests/wink-itest-osgi/wink-itest-simple-osgi/src/test/java/org/apache/wink/itest/osgi/simpleload/WinkClientTest.java
@@ -0,0 +1,69 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *  
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License. 
+ */
+package org.apache.wink.itest.osgi.simpleload;
+
+import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
+import static org.ops4j.pax.exam.CoreOptions.options;
+import static org.ops4j.pax.exam.CoreOptions.provision;
+
+import org.apache.wink.client.RestClient;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.junit.Configuration;
+import org.ops4j.pax.exam.junit.JUnit4TestRunner;
+
+@RunWith(JUnit4TestRunner.class)
+public class WinkClientTest {
+
+	// @Inject
+	// private BundleContext bundleContext;
+
+	@Configuration
+	public static Option[] configuration() {
+		return options(provision(
+
+//		mavenBundle().groupId("commons-lang").artifactId("commons-lang")
+//				.versionAsInProject(),
+
+		mavenBundle().groupId("org.apache.wink").artifactId("wink-client")
+				.versionAsInProject(),
+
+		mavenBundle().groupId("org.apache.wink").artifactId("wink-common")
+				.versionAsInProject(),
+
+		mavenBundle().groupId("javax.ws.rs").artifactId("jsr311-api")
+				.versionAsInProject(),
+
+		mavenBundle().groupId("org.slf4j").artifactId("slf4j-api")
+				.versionAsInProject(),
+
+		mavenBundle().groupId("org.slf4j").artifactId("slf4j-simple")
+				.versionAsInProject()
+				));
+//
+//		mavenBundle().groupId("org.apache.geronimo.specs").artifactId(
+//				"geronimo-servlet_2.5_spec").versionAsInProject()));
+	}
+
+	@Test
+	public void testRestServletResolves() {
+		new RestClient();
+	}
+}
