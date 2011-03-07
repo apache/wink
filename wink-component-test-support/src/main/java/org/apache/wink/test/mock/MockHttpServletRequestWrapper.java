8218303116c1b0354cb55fca253a2298fb915e79
Luciano Resende <lresende@apache.org>
2011-03-07 05:55:03 +0000

    WINK-339 - Applying patch from Raymond Feng to enhance MockHttpServletRequestWrapper to handle getContentType and parameter related methods
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wink/trunk@1078690 13f79535-47bb-0310-9956-ffa450edef68

--- wink-component-test-support/src/main/java/org/apache/wink/test/mock/MockHttpServletRequestWrapper.java
+++ wink-component-test-support/src/main/java/org/apache/wink/test/mock/MockHttpServletRequestWrapper.java
@@ -20,7 +20,16 @@
 
 package org.apache.wink.test.mock;
 
+import java.io.IOException;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.servlet.ServletInputStream;
+import javax.ws.rs.core.MediaType;
 
 import org.springframework.mock.web.MockHttpServletRequest;
 
@@ -43,6 +52,7 @@ public class MockHttpServletRequestWrapper extends MockHttpServletRequest {
             contentType += ";charset=" + getCharacterEncoding();
         }
         addHeader("Content-Type", contentType);
+        super.setContentType(contentType);
     }
 
     @Override
@@ -52,4 +62,119 @@ public class MockHttpServletRequestWrapper extends MockHttpServletRequest {
             addHeader("Content-Length", String.valueOf(content.length));
         }
     }
+
+    public String decode(String s) {
+        try {
+            String encoding = getCharacterEncoding();
+            if (encoding == null) {
+                encoding = "UTF-8";
+            }
+            return URLDecoder.decode(s, encoding);
+            // This implements http://oauth.pbwiki.com/FlexibleDecoding
+        } catch (java.io.UnsupportedEncodingException e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
+
+    /**
+     * Parse a form-urlencoded document.
+     */
+    public Map<String, String> decodeForm(String form) {
+        Map<String, String> params = new HashMap<String, String>();
+        if (form != null && form.length() > 0) {
+            for (String nvp : form.split("\\&")) {
+                int equals = nvp.indexOf('=');
+                String name;
+                String value;
+                if (equals < 0) {
+                    name = decode(nvp);
+                    value = null;
+                } else {
+                    name = decode(nvp.substring(0, equals));
+                    value = decode(nvp.substring(equals + 1));
+                }
+                params.put(name, value);
+            }
+        }
+        return params;
+    }
+
+    /**
+     * Read data from Input Stream and save it as a String.
+     *
+     * @param is InputStream to be read
+     * @return String that was read from the stream
+     * @throws UnsupportedEncodingException 
+     */
+    private String readContent() {
+        Reader ir;
+        try {
+            ir = getReader();
+        } catch (UnsupportedEncodingException e1) {
+            throw new IllegalArgumentException(e1);
+        }
+        if (ir == null) {
+            return null;
+        }
+        StringBuffer sb = new StringBuffer();
+
+        char[] buffer = new char[1024];
+        try {
+            int size = 0;
+            while ((size = ir.read(buffer)) != -1) {
+                sb.append(buffer, 0, size);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String string = sb.toString();
+        return string.trim();
+    }
+
+    private boolean done;
+
+    /**
+     * Read the parameters from the content
+     * @throws UnsupportedEncodingException
+     */
+    private synchronized void readFromForm() {
+        if (done) {
+            return;
+        }
+        String contentType = getContentType();
+        if ("POST".equals(getMethod()) && contentType != null
+            && contentType.startsWith(MediaType.APPLICATION_FORM_URLENCODED)) {
+            String form = readContent();
+            Map<String, String> params = decodeForm(form);
+            for (Map.Entry<String, String> e : params.entrySet()) {
+                addParameter(e.getKey(), e.getValue());
+            }
+        }
+        done = true;
+    }
+
+    @Override
+    public String getParameter(String name) {
+        readFromForm();
+        return super.getParameter(name);
+    }
+
+    @Override
+    public Enumeration getParameterNames() {
+        readFromForm();
+        return super.getParameterNames();
+    }
+
+    @Override
+    public String[] getParameterValues(String name) {
+        readFromForm();
+        return super.getParameterValues(name);
+    }
+
+    @Override
+    public Map getParameterMap() {
+        readFromForm();
+        return super.getParameterMap();
+    }
+
 }
